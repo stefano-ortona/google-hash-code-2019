3e51dcec16245f1a2b259337919f6bc35750d2e0
Daniele Salvatore <danieleslvtr@gmail.com>
2019-02-28 20:01:29 +0100

    Skeleton Reader

---
+++ src/main/java/com/ortona/stefano/hashcode_2019/UtilsFile.java
@@ -0,0 +1,174 @@
+package com.ortona.stefano.hashcode_2019;
+
+import com.ortona.stefano.hashcode_2019.model.Photo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UtilsFile {
+
+    /* If file structure is preserved (i.e. first line header, rest of the file data)
+        Edit this file as following:
+        1. define type of header items and data items
+        2. generate setters and getters for header and data
+        3. define logic of createHeader() and createData()
+     */
+
+    // 1. define type of header items and data items
+    private int[] header;
+    private char[][] data;
+    private int photoAmount;
+   private List<Photo> photos;
+
+    // 2. generate setters and getters for header and data
+    public void setHeader(int[] header) {
+        this.header = header;
+    }
+
+    public void setData(char[][] data) {
+        this.data = data;
+    }
+
+    public int[] getHeader() {
+        return header;
+    }
+
+    public char[][] getData() {
+        return data;
+    }
+
+    public int getPhotoAmount() {
+        return photoAmount;
+    }
+
+    public void setPhotoAmount(int photoAmount) {
+        this.photoAmount = photoAmount;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    //3. define logic of createHeader() and createData()
+
+    public void createHeader() {
+        String firstLine = getFirstLineOfFile();
+        String[] split = splitString(firstLine, " ");
+        int[] converted = convertArrayOfStringToArrayOfInt(split);
+
+        // always finish with this.setHeader()
+        this.setHeader(converted);
+    }
+
+    public void createData() {
+        String[] file = this.getFile();
+        String[] dataRaw = cloneArrayOfString(file, 1, file.length);
+        char[][] matrix = convertArrayOfStringToArrayOfCharArrays(dataRaw);
+
+        // always finish with this.setData()
+        this.setData(matrix);
+    }
+
+
+
+
+
+    // ====== Do not change below here
+
+    private static final String RESOURCE_PATH = "src/main/resources/com.ortona.stefano.hashcode_2019/";
+    private String[] file;
+
+    public void setFile(String[] file) {
+        this.file = file;
+    }
+
+    public String[] getFile() {
+        return file;
+    }
+
+    // Constructor
+    public UtilsFile(String filepath) {
+
+        try {
+            File file = new File(RESOURCE_PATH + filepath);
+            String absolutePath = file.getAbsolutePath();
+
+            readFile(absolutePath);
+
+            createHeader();
+
+            this.setPhotoAmount(this.getHeader()[0]);
+
+            createData();
+
+            //createPhotos();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    // Utils
+    public void readFile(String filepath) throws IOException {
+
+        String line;
+        List<String> list = new ArrayList<>();
+        BufferedReader in = new BufferedReader(new FileReader(filepath));
+
+        while ((line = in.readLine()) != null) {
+            list.add(line);  //Add line to file
+        }
+
+        //Cast List to Array of Array
+        String[] stringArr = list.toArray(new String[0]);
+
+        this.setFile(stringArr);
+
+    }
+
+    public String getFirstLineOfFile() {
+        return this.file[0];
+    }
+
+    public String[] splitString(String string, String separator) {
+        return string.split(separator);
+    }
+
+    public String[] cloneArrayOfString(String[] source, Integer start, Integer to) {
+        return Arrays.copyOfRange(source, start, to);
+    }
+
+    public char[] convertStringToArrayOfChar(String string) {
+        return string.toCharArray();
+    }
+
+    public char[][] convertArrayOfStringToArrayOfCharArrays(String[] dataRaw) {
+
+        char[][] result = new char[dataRaw.length][dataRaw[0].length()];
+
+        for (int i = 0; i < dataRaw.length; i++) {
+            result[i] = convertStringToArrayOfChar(dataRaw[i]);
+        }
+        return result;
+    }
+
+    public String convertArrayOfChartToString(char[] a) {
+        return new String(a);
+    }
+
+    public int[] convertArrayOfStringToArrayOfInt(String[] strings) {
+        return Arrays.asList(strings).stream().mapToInt(Integer::parseInt).toArray();
+
+    }
+
+}
---
+++ src/test/java/com/ortona/stefano/hashcode_2019/UtilsFileTest.java
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2016
+ */
+package com.ortona.stefano.hashcode_2019;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test
+ */
+public class UtilsFileTest {
+
+    private final String filePathExample = "a_example.txt";
+    private UtilsFile fr = new UtilsFile(filePathExample);
+    private int[] headerExample = fr.getHeader();
+    private int[] actualHeaderExample = new int[]{4};
+
+    // Example.in file
+
+    @Test
+    public void testHeaderExample() {
+        Assert.assertEquals(true, true);
+
+    }
+
+    // utils
+
+    private void testDataLineByLine(String[] actualData, char[][] data) {
+
+        // line by line
+        for (int i = 0; i < actualData.length; i++) {
+            String d = fr.convertArrayOfChartToString(data[i]);
+            Assert.assertEquals(d, actualData[i]);
+        }
+
+    }
+
+    private void testHeader(int[] actualHeader, int[] header) {
+        for (int i = 0; i < actualHeader.length; i++) {
+            Assert.assertEquals(header[i], actualHeader[i]);
+        }
+    }
+
+    private void testDataCharByChar(String[] actualData, char[][] data) {
+
+        for (int i = 0; i < actualData.length; i++) {
+            testCharByChartLine(actualData[i], data[i]);
+        }
+
+    }
+
+    private void testCharByChartLine(String line, char[] data) {
+
+        for (int i = 0; i < line.length(); i++) {
+            Assert.assertEquals(line.charAt(i), data[i]);
+        }
+
+    }
+}
