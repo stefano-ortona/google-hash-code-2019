b47ac583b11181a467ec440fb90f4b734da3bea1
Carlo Luchessa <c.luchessa@reply.it>
2019-02-28 21:48:49 +0100

    up.

--- src/main/java/com/ortona/stefano/hashcode_2019/logic/ComputeBestPictureGroups.java
+++ src/main/java/com/ortona/stefano/hashcode_2019/logic/ComputeBestPictureGroups.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import com.ortona.stefano.hashcode_2019.logic.interfaces.IComputeBestPictureGroups;
 import com.ortona.stefano.hashcode_2019.logic.utils.CommonUtils;
@@ -12,74 +14,80 @@ import com.sun.istack.internal.NotNull;
 
 public class ComputeBestPictureGroups implements IComputeBestPictureGroups {
 
-	private final int maxNumPhoto;
+    private final int maxNumPhoto;
 
-	public ComputeBestPictureGroups(int maxNumPhoto) {
-		this.maxNumPhoto = maxNumPhoto;
-	}
+    public ComputeBestPictureGroups(int maxNumPhoto) {
+        this.maxNumPhoto = maxNumPhoto;
+    }
 
-	@Override
-	public List<Photo> getBestGroup(Set<String> tags, @NotNull List<Photo> allPics) {
-		if ((tags == null) || tags.isEmpty()) {
-			return getFirstPhotoList(allPics);
-		}
-		return getPhotoList(tags, allPics, maxNumPhoto);
-	}
+    @Override
+    public List<Photo> getBestGroup(Set<String> tags, @NotNull List<Photo> allPics) {
+        if ((tags == null) || tags.isEmpty()) {
+            return getFirstPhotoList(allPics);
+        }
+        return getPhotoList(tags, allPics, maxNumPhoto);
+    }
 
-	/*
-	 * Internal methods
-	 */
+    /*
+     * Internal methods
+     */
 
-	private List<Photo> getFirstPhotoList(@NotNull List<Photo> allPics) {
-		final Photo photo = getBestPhotoEvah(allPics);
-		allPics.remove(photo);
-		final List<Photo> bestPhotoList = getPhotoList(photo.getTags(), allPics, maxNumPhoto - 1);
-		bestPhotoList.add(photo);
-		return bestPhotoList;
-	}
+    private List<Photo> getFirstPhotoList(@NotNull List<Photo> allPics) {
+        final Photo photo = getBestPhotoEvah(allPics);
+        allPics.remove(photo);
+        final List<Photo> bestPhotoList = getPhotoList(photo.getTags(), allPics, maxNumPhoto - 1);
+        bestPhotoList.add(photo);
+        return bestPhotoList;
+    }
 
-	private List<Photo> getPhotoList(@NotNull Set<String> tags, @NotNull List<Photo> allPics, int tot) {
-		final TreeSet<PhotoScore> target = new TreeSet<>();
-		for (final Photo p : allPics) {
-			final PhotoScore pS = new PhotoScore();
-			pS.p = p;
-			pS.score = CommonUtils.computeScore(tags, p.getTags());
-			if (target.size() < tot) {
-				target.add(pS);
-			} else {
-				target.remove(target.last());
-				target.add(pS);
-			}
-		}
-		final List<Photo> output = new ArrayList<>();
-		target.forEach(pS -> output.add(pS.p));
-		return output;
-	}
+    private List<Photo> getPhotoList(@NotNull Set<String> tags, @NotNull List<Photo> allPics, int tot) {
+        final TreeSet<PhotoScore> target = new TreeSet<>();
+        for (final Photo p : allPics) {
+            final PhotoScore pS = new PhotoScore();
+            pS.p = p;
+            pS.score = CommonUtils.computeScore(tags, p.getTags());
+            if (target.size() < tot) {
+                target.add(pS);
+            } else {
+                target.remove(target.last());
+                target.add(pS);
+            }
+        }
+        final List<Photo> output = new ArrayList<>();
 
-	private class PhotoScore implements Comparable<PhotoScore> {
-		long score;
-		Photo p;
+        Stream<Photo> outputStream = output.stream().filter(Photo::isVertical);
+        int nVertical = outputStream.toArray().length;
+        if (nVertical % 2 != 0 && nVertical > 0) {
+            output.remove(outputStream.findFirst().get());
+        }
+        target.forEach(pS -> output.add(pS.p));
+        return output;
+    }
 
-		@Override
-		public int compareTo(PhotoScore o) {
-			return (int) (o.score - this.score);
-		}
-	}
+    private class PhotoScore implements Comparable<PhotoScore> {
+        long score;
+        Photo p;
 
-	/*
-	 * Utility methods
-	 */
+        @Override
+        public int compareTo(PhotoScore o) {
+            return (int) (o.score - this.score);
+        }
+    }
 
-	private Photo getBestPhotoEvah(List<Photo> allPics) {
-		Photo bestPhoto = null;
-		int tagsSize = 0;
-		for (final Photo photo : allPics) {
-			if (photo.getTags().size() > tagsSize) {
-				bestPhoto = photo;
-				tagsSize = photo.getTags().size();
-			}
-		}
-		return bestPhoto;
-	}
+    /*
+     * Utility methods
+     */
+
+    private Photo getBestPhotoEvah(List<Photo> allPics) {
+        Photo bestPhoto = null;
+        int tagsSize = 0;
+        for (final Photo photo : allPics) {
+            if (photo.getTags().size() > tagsSize) {
+                bestPhoto = photo;
+                tagsSize = photo.getTags().size();
+            }
+        }
+        return bestPhoto;
+    }
 
 }
