f351825337e869144adc34e36ea13e9334a1538d
Carlo Luchessa <c.luchessa@reply.it>
2019-02-28 20:00:22 +0100

    up.

--- src/main/java/com/ortona/stefano/hashcode_2019/logic/ComputeBestPictureGroups.java
+++ src/main/java/com/ortona/stefano/hashcode_2019/logic/ComputeBestPictureGroups.java
@@ -1,5 +1,7 @@
 package com.ortona.stefano.hashcode_2019.logic;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
@@ -7,10 +9,39 @@ import com.ortona.stefano.hashcode_2019.model.Photo;
 
 public class ComputeBestPictureGroups {
 
-	int MAX_NUM_PHOTO = 10;
+    int MAX_NUM_PHOTO = 10;
 
-	public List<Photo> getBestGroup(Set<String> tags, List<Photo> allPics) {
+    public List<Photo> getBestGroup(Set<String> tags, List<Photo> allPics) {
+        if (tags == null) {
+            return getFirstPhotoList(allPics);
+        }
+        return getPhotoList(tags, allPics, MAX_NUM_PHOTO);
+    }
 
-	}
+
+    /*
+     * Internal methods
+     */
+
+    private List<Photo> getFirstPhotoList(List<Photo> allPics) {
+        Photo photo = getBestPhotoEvah(allPics);
+        //getPhotoList()
+
+        return null;
+    }
+
+    private List<Photo> getPhotoList(Set<String> tags, List<Photo> allPics, int tot) {
+        return null;
+    }
+
+
+    /*
+     * Utility methods
+     */
+
+    private Photo getBestPhotoEvah(List<Photo> allPics) {
+        allPics.sort((o1, o2) -> o2.getTags().size() - o1.getTags().size());
+        return allPics.get(0); // >> tags
+    }
 
 }
